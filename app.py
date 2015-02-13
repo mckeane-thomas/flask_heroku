70e806b6e90ccc2b36c061d7e78f695a699d5819
mckeane-thomas <mckeane.thomas@gmail.com>
2015-02-13 12:39:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ This file creates your application.
 """
 
 import os
-from Flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
